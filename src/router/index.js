2910ae449d6bd2970be80f775bdd8a64fac6fa97
yizlee <7961468@qq.com>
2018-06-09 22:30:39 +0800

    first

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Goods from '@/components/goods'
+import Sellers from '@/components/sellers'
+import Ratings from '@/components/ratings'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Goods',
+      component: Goods
+    },{
+      path: '/goods',
+      component: Goods
+    },{
+      path: '/ratings',
+      component: Ratings
+    },{
+      path: '/sellers',
+      component: Sellers
+    }
+  ]
+})
